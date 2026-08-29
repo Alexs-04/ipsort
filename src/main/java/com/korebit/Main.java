82484e24aec9ebfdf9d982a3b5151036bc9efbb7
Alexs-04 <beltranrojoalexs@gmail.com>
2026-08-28 23:54:16 -0600

    feat: AlfaUtils added

--- src/main/java/com/korebit/Main.java
+++ src/main/java/com/korebit/Main.java
@@ -1,11 +1,9 @@
 package com.korebit;
 
-import com.korebit.model.Data;
 import com.korebit.view.LoginScreen;
 
 public class Main {
     public static void main(String[] args) {
-        Data.printTheFirstHundredNetworks();
-//        new LoginScreen().setVisible(true);
+        new LoginScreen().setVisible(true);
     }
 }
